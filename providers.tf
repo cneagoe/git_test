d8b2e38ef18e8b246ce5847cda3f421a31e3cd3c
cip <ng.ciprian@gmail.com>
2024-05-14 19:10:34 +0300

    added key

--- providers.tf
+++ providers.tf
@@ -3,6 +3,8 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      access_key = "AKIARLKB4QXV7DPLGP2Q"
+      secret_key = "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"
     }
   }
 }
