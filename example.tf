8bbd36a334e062bf0660017a0ab06be1f57ef204
cip <ng.ciprian@gmail.com>
2024-05-14 19:15:31 +0300

    added key

--- example.tf
+++ example.tf
@@ -1,6 +1,8 @@
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIARLKB4QXV7DPLGP2Q"
+  secret_key = "VFaAD+J/XCudS1lHKIWDoEyvJ2XTRk93LJWVp3c2"
 }
 
 # Create a VPC
--- providers.tf
+++ providers.tf
@@ -3,8 +3,6 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
-      access_key = "AKIARLKB4QXV7DPLGP2Q"
-      secret_key = "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"
     }
   }
 }
