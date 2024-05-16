688047abe6c4a6c6158d2247a3a2b50f5bc350ab
cip <ng.ciprian@gmail.com>
2024-05-16 19:49:49 +0300

    changed owner id

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "default" {
     allocated_storage    = 20
     engine               = "mysql"
     engine_version       = "8.0"
-    instance_class       = "db.t2.micro"
+    instance_class       = "db.t3.micro"
     identifier           = var.db_name
     username             = var.db_username
     password             = var.db_password
